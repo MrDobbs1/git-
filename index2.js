caa6cffd862d5c38ad0aa93c9231af6d85ac7a18
Chris Tobar <christobar697@hotmail.com>
2025-11-06 02:37:23 -0500

    added function to index2.js

--- index2.js
+++ index2.js
@@ -1,6 +1,14 @@
-function addNumbers(a, b) {
+/*function addNumbers(a, b) {
 const add = a + b;
 return add;
 }
 
-console.log(addNumbers(2, 2));
+console.log(addNumbers(2, 2)); */
+
+
+function minusNumbers(a, b) {
+const sub = a - b;
+return sub;
+}
+
+console.log(minusNumbers(10, 5));
